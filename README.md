5e808fef0f5717c8751cb9065b1fed62e09a9c1b
meilinlyu <meilin.lyu@mail.mcgill.ca>
2023-11-22 14:30:02 -0500

    added iteratively lookback feature

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
+Input the keyword you want to use in test.json
+
 Navigate to newscover folder
 Python3 collector.py -k d89a27d952a942c0bef7d7dc7c28626a -b 31 -i test.json -o output
+- use your own api key after -k
 
 run processor.py 
 - to get the excel file with all 500 articles and respective features (source:name, title, description)
@@ -12,4 +15,5 @@ The sources[country].json file
 - contains news sources in us and ca
 - which are the two countries representing North America
 - these sources are passed as inputs to get articles
+
 
--- newscover/newsapi.py
+++ newscover/newsapi.py
@@ -3,28 +3,29 @@ import datetime
 import re
 
 
-base_url = "https://newsapi.org/v2/top-headlines/sources"
-api_key = "d89a27d952a942c0bef7d7dc7c28626a"
 
-params = {
-    "language": "en",
-    "apiKey": api_key,
-    #"page": 1, #altered from 1 to 5 to collect result from 5 pages
-    #"sources":
-    "country": "us, ca"
-}
-def fetch_latest_news(api_key, news_keywords, lookback_days=31):
+def fetch_latest_news(api_key, news_keywords, start_date, end_date):
     #if re.search(r'[^a-zA-Z]', news_keywords) is not None:
         #raise ValueError()
     
     base_url = "https://newsapi.org/v2/everything/"
 
     #lookback day by day, I think 5 pages is the maximum for display
+    # params = {
+    #     "q": news_keywords,
+    #     "language": "en",
+    #     "from": (datetime.datetime.now() - datetime.timedelta(days=lookback_days)).isoformat(),
+    #     "to": datetime.datetime.now().isoformat(),
+    #     "apiKey": api_key,
+    #     #"page": 1, #altered from 1 to 5 to collect result from 5 pages
+    #     #"sources":
+
+    # }
     params = {
         "q": news_keywords,
         "language": "en",
-        "from": (datetime.datetime.now() - datetime.timedelta(days=lookback_days)).isoformat(),
-        "to": datetime.datetime.now().isoformat(),
+        "from": start_date, #"2023-11-22"
+        "to": end_date,
         "apiKey": api_key,
         #"page": 1, #altered from 1 to 5 to collect result from 5 pages
         #"sources":
