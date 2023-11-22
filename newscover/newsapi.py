21ba4da13863bdffa460aea037b9852b1f7c7087
meilinlyu <meilin.lyu@mail.mcgill.ca>
2023-11-22 12:29:01 -0500

    update

--- newscover/newsapi.py
+++ newscover/newsapi.py
@@ -2,18 +2,32 @@ import requests
 import datetime
 import re
 
+
+base_url = "https://newsapi.org/v2/top-headlines/sources"
+api_key = "d89a27d952a942c0bef7d7dc7c28626a"
+
+params = {
+    "language": "en",
+    "apiKey": api_key,
+    #"page": 1, #altered from 1 to 5 to collect result from 5 pages
+    #"sources":
+    "country": "us, ca"
+}
 def fetch_latest_news(api_key, news_keywords, lookback_days=31):
     #if re.search(r'[^a-zA-Z]', news_keywords) is not None:
         #raise ValueError()
     
-    base_url = "https://newsapi.org/v2/everything"
+    base_url = "https://newsapi.org/v2/everything/"
+
+    #lookback day by day, I think 5 pages is the maximum for display
     params = {
         "q": news_keywords,
         "language": "en",
         "from": (datetime.datetime.now() - datetime.timedelta(days=lookback_days)).isoformat(),
         "to": datetime.datetime.now().isoformat(),
         "apiKey": api_key,
-        "page": 5 #altered from 1 to 5 to collect result from 5 pages
+        #"page": 1, #altered from 1 to 5 to collect result from 5 pages
+        #"sources":
 
     }
 
